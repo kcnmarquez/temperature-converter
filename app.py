85901202e0a3a0d7827ca32f2fa3efdb9ab3546c
Kirzten Marquez <kacimarquez+github@gmail.com>
2022-05-23 12:06:19 +0800

    - Add rate limit to API - Add `Flask-Limiter` to requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_restful import Resource, Api, reqparse
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 from temperature_converter import TemperatureConverter
 
@@ -8,12 +10,19 @@ def create_app():
     app = Flask(__name__)
     Bootstrap(app)
     api = Api(app)
+    limiter = Limiter(
+        app,
+        key_func=get_remote_address,
+        default_limits=["200 per day", "50 per hour"]
+    )
 
     parser = reqparse.RequestParser()
     parser.add_argument("value")
     parser.add_argument("desired_unit")
 
     class ConvertTemperature(Resource):
+        decorators = [limiter.limit("10/minute", override_defaults=False)]
+
         def post(self):
             args = parser.parse_args()
             return {
